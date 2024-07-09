6220bbe3302db5faa6987819bec611513a9aa570
Ajith-11399 <ajithrs45@gmail.com>
2024-07-10 00:15:46 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./Database/config.js";
-import userRoute from "../Backend/Routes/userRoute.js";
+import userRoute from "./Routes/userRoute.js";
 
 dotenv.config();
 
